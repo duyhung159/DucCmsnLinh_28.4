07c97f6433979c762cfa2db29513f0c65fde06a0
unknown <tyghu105@gmail.com>
2022-04-28 21:11:41 +0700

    as

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 $("#login-button").click(function (event) {
     var userName = document.getElementById("userName").value;
     var pwd = document.getElementById("pwd").value;
-    if (userName == "yeuEm" && pwd == "123") {
+    if (userName == "TDiệp Linh" && pwd == "linhduc") {
       event.preventDefault();
       $("form").fadeOut(500);
       $(".wrapper").addClass("form-success");
