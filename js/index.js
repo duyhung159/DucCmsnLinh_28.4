6cf5aaa107d48674fdd9c1ac7dfd6b640f040aef
duyhung159 <83937216+duyhung159@users.noreply.github.com>
2022-04-28 21:13:35 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 $("#login-button").click(function (event) {
     var userName = document.getElementById("userName").value;
     var pwd = document.getElementById("pwd").value;
-    if (userName == "TDiệp Linh" && pwd == "linhduc") {
+    if (userName == "TDiepLinh" && pwd == "linhduc") {
       event.preventDefault();
       $("form").fadeOut(500);
       $(".wrapper").addClass("form-success");
